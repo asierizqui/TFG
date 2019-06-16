99934651979ea147a71744a01b6498dba53f6503
Asier Izquierdo <asierizqui@gmail.com>
2019-06-16 23:46:09 +0200

    Add files via upload

---
+++ QAL_output.m
@@ -0,0 +1,462 @@
+________________________________GRID at t=0______________________________________
+   0   0   0   0
+   0   0   2   3
+   1   0   0   0
+
+
+
+-------------------------------------At t=0--------------------------------------
+
+
+____________________________________AGE___________________________________
+Age at t=0
+     for individual 1:  <sigma_z>_p0=-1.2
+     for individual 2:  <sigma_z>_p0=-0.2
+     for individual 3:  <sigma_z>_p0=-2.6
+
+
+
+____________________________Random MOVEMENT_______________________________
+NO movement at t=0.
+
+
+____________________________ INTERACTION__________________________________
+Individual 1 interacts with individual 3.
+     Age at t=0,    (<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+           for individual 1:  <sigma_z>_p0=-1.2
+           for individual 3:  <sigma_z>_p0=-2.6
+     Thus, here: 1=predator; 3=prey.
+
+Individual 2 interacts with individual 3.
+     Age at t=0,    (<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+           for individual 2:  <sigma_z>_p0=-0.2
+           for individual 3:  <sigma_z>_p0=-2.6
+     Thus, here: 2=predator; 3=prey.
+
+Individual 3 interacts with individual 2.
+     Age at t=0,    (<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+           for individual 3:  <sigma_z>_p0=-2.6
+           for individual 2:  <sigma_z>_p0=-0.2
+     Thus, here: 2=predator; 3=prey.
+
+Individual 3 interacts with individual 1.
+     Age at t=0,    (<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+           for individual 3:  <sigma_z>_p0=-2.6
+           for individual 1:  <sigma_z>_p0=-1.2
+     Thus, here: 1=predator; 3=prey.
+
+
+
+Unrepeated interactions:
+       Interaction number 1: individual 1 is the predator; individual 3, the prey.
+       Interaction number 2: individual 2 is the predator; individual 3, the prey.
+
+
+
+
+
+
+
+
+
+
+-------------------------------------At t=0.1--------------------------------------
+
+
+____________________________________AGE___________________________________
+Age at t=0.1
+     for individual 1:  <sigma_z>_p0=-0.99064
+     for individual 2:  <sigma_z>_p0=-0.085805
+     for individual 3:  <sigma_z>_p0=-2.2574
+
+
+
+____________________________Random MOVEMENT_______________________________
+If individual 1 moves in direction (x,y)=(-1,1):
+Individual 1 canNOT move there, that cell is already occupied by individual 3.
+   0   0   0   0
+   0   0   2   3
+   1   0   0   0
+If individual 2 moves in direction (x,y)=(-1,0):
+   0   0   0   0
+   0   2   0   3
+   1   0   0   0
+If individual 3 moves in direction (x,y)=(0,1):
+   0   0   0   3
+   0   2   0   0
+   1   0   0   0
+
+
+____________________________ INTERACTION__________________________________
+Individual 1 interacts with individual 3.
+     Age at t=0,    (<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+           for individual 1:  <sigma_z>_p0=-1.2
+           for individual 3:  <sigma_z>_p0=-2.6
+     Thus, here: 1=predator; 3=prey.
+
+Individual 1 interacts with individual 2.
+     Age at t=0,    (<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+           for individual 1:  <sigma_z>_p0=-1.2
+           for individual 2:  <sigma_z>_p0=-0.2
+     Thus, here: 2=predator; 1=prey.
+
+Individual 2 interacts with individual 1.
+     Age at t=0,    (<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+           for individual 2:  <sigma_z>_p0=-0.2
+           for individual 1:  <sigma_z>_p0=-1.2
+     Thus, here: 2=predator; 1=prey.
+
+Individual 3 interacts with individual 1.
+     Age at t=0,    (<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+           for individual 3:  <sigma_z>_p0=-2.6
+           for individual 1:  <sigma_z>_p0=-1.2
+     Thus, here: 1=predator; 3=prey.
+
+
+
+Unrepeated interactions:
+       Interaction number 1: individual 1 is the predator; individual 3, the prey.
+       Interaction number 2: individual 2 is the predator; individual 1, the prey.
+
+
+
+
+
+
+
+
+
+
+-------------------------------------At t=0.2--------------------------------------
+
+
+____________________________________AGE___________________________________
+Age at t=0.2
+     for individual 1:  <sigma_z>_p0=-0.6298
+     for individual 2:  <sigma_z>_p0=0.11102
+     for individual 3:  <sigma_z>_p0=-1.6669
+
+
+
+____________________________Random MOVEMENT_______________________________
+If individual 1 moves in direction (x,y)=(0,1):
+   0   0   0   3
+   1   2   0   0
+   0   0   0   0
+If individual 2 moves in direction (x,y)=(0,0):
+   0   0   0   3
+   1   2   0   0
+   0   0   0   0
+If individual 3 moves in direction (x,y)=(1,1):
+   0   0   0   0
+   1   2   0   0
+   3   0   0   0
+
+
+____________________________ INTERACTION__________________________________
+Individual 1 interacts with individual 3.
+     Age at t=0,    (<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+           for individual 1:  <sigma_z>_p0=-1.2
+           for individual 3:  <sigma_z>_p0=-2.6
+     Thus, here: 1=predator; 3=prey.
+
+Individual 1 interacts with individual 2.
+     Age at t=0,    (<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+           for individual 1:  <sigma_z>_p0=-1.2
+           for individual 2:  <sigma_z>_p0=-0.2
+     Thus, here: 2=predator; 1=prey.
+
+Individual 2 interacts with individual 3.
+     Age at t=0,    (<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+           for individual 2:  <sigma_z>_p0=-0.2
+           for individual 3:  <sigma_z>_p0=-2.6
+     Thus, here: 2=predator; 3=prey.
+
+Individual 2 interacts with individual 1.
+     Age at t=0,    (<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+           for individual 2:  <sigma_z>_p0=-0.2
+           for individual 1:  <sigma_z>_p0=-1.2
+     Thus, here: 2=predator; 1=prey.
+
+Individual 3 interacts with individual 1.
+     Age at t=0,    (<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+           for individual 3:  <sigma_z>_p0=-2.6
+           for individual 1:  <sigma_z>_p0=-1.2
+     Thus, here: 1=predator; 3=prey.
+
+Individual 3 interacts with individual 2.
+     Age at t=0,    (<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+           for individual 3:  <sigma_z>_p0=-2.6
+           for individual 2:  <sigma_z>_p0=-0.2
+     Thus, here: 2=predator; 3=prey.
+
+
+
+Unrepeated interactions:
+       Interaction number 1: individual 1 is the predator; individual 3, the prey.
+       Interaction number 2: individual 2 is the predator; individual 1, the prey.
+       Interaction number 3: individual 2 is the predator; individual 3, the prey.
+
+
+
+
+
+
+
+
+
+
+-------------------------------------At t=0.3--------------------------------------
+
+
+____________________________________AGE___________________________________
+Age at t=0.3
+     for individual 1:  <sigma_z>_p0=-0.20739
+     for individual 2:  <sigma_z>_p0=0.34143
+     for individual 3:  <sigma_z>_p0=-0.97572
+
+
+
+____________________________Random MOVEMENT_______________________________
+If individual 1 moves in direction (x,y)=(0,1):
+   1   0   0   0
+   0   2   0   0
+   3   0   0   0
+If individual 2 moves in direction (x,y)=(0,-1):
+   1   0   0   0
+   0   0   0   0
+   3   2   0   0
+If individual 3 moves in direction (x,y)=(-1,0):
+   1   0   0   0
+   0   0   0   0
+   0   2   0   3
+
+
+____________________________ INTERACTION__________________________________
+Individual 1 interacts with individual 3.
+     Age at t=0,    (<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+           for individual 1:  <sigma_z>_p0=-1.2
+           for individual 3:  <sigma_z>_p0=-2.6
+     Thus, here: 1=predator; 3=prey.
+
+Individual 1 interacts with individual 2.
+     Age at t=0,    (<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+           for individual 1:  <sigma_z>_p0=-1.2
+           for individual 2:  <sigma_z>_p0=-0.2
+     Thus, here: 2=predator; 1=prey.
+
+Individual 2 interacts with individual 1.
+     Age at t=0,    (<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+           for individual 2:  <sigma_z>_p0=-0.2
+           for individual 1:  <sigma_z>_p0=-1.2
+     Thus, here: 2=predator; 1=prey.
+
+Individual 3 interacts with individual 1.
+     Age at t=0,    (<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+           for individual 3:  <sigma_z>_p0=-2.6
+           for individual 1:  <sigma_z>_p0=-1.2
+     Thus, here: 1=predator; 3=prey.
+
+
+
+Unrepeated interactions:
+       Interaction number 1: individual 1 is the predator; individual 3, the prey.
+       Interaction number 2: individual 2 is the predator; individual 1, the prey.
+
+
+
+
+
+
+
+
+
+
+-------------------------------------At t=0.4--------------------------------------
+
+
+____________________________________AGE___________________________________
+Age at t=0.4
+     for individual 1:  <sigma_z>_p0=0.19067
+     for individual 2:  <sigma_z>_p0=0.55854
+     for individual 3:  <sigma_z>_p0=-0.32437
+
+
+
+____________________________Random MOVEMENT_______________________________
+If individual 1 moves in direction (x,y)=(-1,1):
+Individual 1 canNOT move there, that cell is already occupied by individual 3.
+   1   0   0   0
+   0   0   0   0
+   0   2   0   3
+If individual 2 moves in direction (x,y)=(-1,0):
+   1   0   0   0
+   0   0   0   0
+   2   0   0   3
+If individual 3 moves in direction (x,y)=(1,1):
+   1   0   0   0
+   3   0   0   0
+   2   0   0   0
+
+
+____________________________ INTERACTION__________________________________
+Individual 1 interacts with individual 3.
+     Age at t=0,    (<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+           for individual 1:  <sigma_z>_p0=-1.2
+           for individual 3:  <sigma_z>_p0=-2.6
+     Thus, here: 1=predator; 3=prey.
+
+Individual 1 interacts with individual 2.
+     Age at t=0,    (<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+           for individual 1:  <sigma_z>_p0=-1.2
+           for individual 2:  <sigma_z>_p0=-0.2
+     Thus, here: 2=predator; 1=prey.
+
+Individual 2 interacts with individual 1.
+     Age at t=0,    (<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+           for individual 2:  <sigma_z>_p0=-0.2
+           for individual 1:  <sigma_z>_p0=-1.2
+     Thus, here: 2=predator; 1=prey.
+
+Individual 2 interacts with individual 3.
+     Age at t=0,    (<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+           for individual 2:  <sigma_z>_p0=-0.2
+           for individual 3:  <sigma_z>_p0=-2.6
+     Thus, here: 2=predator; 3=prey.
+
+Individual 3 interacts with individual 2.
+     Age at t=0,    (<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+           for individual 3:  <sigma_z>_p0=-2.6
+           for individual 2:  <sigma_z>_p0=-0.2
+     Thus, here: 2=predator; 3=prey.
+
+Individual 3 interacts with individual 1.
+     Age at t=0,    (<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+           for individual 3:  <sigma_z>_p0=-2.6
+           for individual 1:  <sigma_z>_p0=-1.2
+     Thus, here: 1=predator; 3=prey.
+
+
+
+Unrepeated interactions:
+       Interaction number 1: individual 1 is the predator; individual 3, the prey.
+       Interaction number 2: individual 2 is the predator; individual 1, the prey.
+       Interaction number 3: individual 2 is the predator; individual 3, the prey.
+
+
+
+
+
+
+
+
+
+
+-------------------------------------At t=0.5--------------------------------------
+
+
+____________________________________AGE___________________________________
+Age at t=0.5
+     for individual 1:  <sigma_z>_p0=0.50911
+     for individual 2:  <sigma_z>_p0=0.73224
+     for individual 3:  <sigma_z>_p0=0.19673
+
+Individual 2 has DIED!    (Death age=0.7)
+Then, individual 2 disappears from the grid:
+   1   0   0   0
+   3   0   0   0
+   0   0   0   0
+
+
+
+____________________________Random MOVEMENT_______________________________
+If individual 1 moves in direction (x,y)=(-1,1):
+   0   0   0   0
+   3   0   0   0
+   0   0   0   1
+If individual 3 moves in direction (x,y)=(-1,-1):
+Individual 3 canNOT move there, that cell is already occupied by individual 1.
+   0   0   0   0
+   3   0   0   0
+   0   0   0   1
+
+
+____________________________ INTERACTION__________________________________
+Individual 1 interacts with individual 3.
+     Age at t=0,    (<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+           for individual 1:  <sigma_z>_p0=-1.2
+           for individual 3:  <sigma_z>_p0=-2.6
+     Thus, here: 1=predator; 3=prey.
+
+Individual 3 interacts with individual 1.
+     Age at t=0,    (<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+           for individual 3:  <sigma_z>_p0=-2.6
+           for individual 1:  <sigma_z>_p0=-1.2
+     Thus, here: 1=predator; 3=prey.
+
+
+
+Unrepeated interactions:
+       Interaction number 1: individual 1 is the predator; individual 3, the prey.
+
+
+
+
+
+
+
+
+
+
+-------------------------------------At t=0.6--------------------------------------
+
+
+____________________________________AGE___________________________________
+Age at t=0.6
+     for individual 1:  <sigma_z>_p0=0.7306
+     for individual 3:  <sigma_z>_p0=0.55916
+
+Individual 1 has DIED!    (Death age=0.7)
+Then, individual 1 disappears from the grid:
+   0   0   0   0
+   3   0   0   0
+   0   0   0   0
+
+
+
+____________________________Random MOVEMENT_______________________________
+If individual 3 moves in direction (x,y)=(1,1):
+   0   3   0   0
+   0   0   0   0
+   0   0   0   0
+
+
+____________________________ INTERACTION__________________________________
+
+
+Unrepeated interactions:
+
+
+
+
+
+
+
+
+
+
+-------------------------------------At t=0.7--------------------------------------
+
+
+____________________________________AGE___________________________________
+Age at t=0.7
+     for individual 3:  <sigma_z>_p0=0.78108
+
+Individual 3 has DIED!    (Death age=0.7)
+Then, individual 3 disappears from the grid:
+   0   0   0   0
+   0   0   0   0
+   0   0   0   0
+
+
+ALL individuals are dead, program ENDS!
