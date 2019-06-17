1a6bb193c8a277ecae34dd3201479b0bbe51e2ff
Asier Izquierdo <asierizqui@gmail.com>
2019-06-17 11:08:54 +0200

    Add files via upload

---
+++ QAL_output.m
@@ -0,0 +1,399 @@
+________________________________GRID at t=0______________________________________
+   3   2   0   1
+   0   0   0   0
+   0   0   0   0
+
+
+
+
+
+
+
+-------------------------------------At t=0--------------------------------------
+
+
+
+____________________________________AGE___________________________________
+     1:  <sigma_z>_p0=-2.8
+     2:  <sigma_z>_p0=-0.6
+     3:  <sigma_z>_p0=0.2
+
+
+____________________________Random MOVEMENT_______________________________
+     NO movement at t=0.
+
+
+____________________________ INTERACTION__________________________________
+(<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+
+Individual 1 interacts with individual 3.
+     1:  <sigma_z>_p0(t=0)=-2.8
+     3:  <sigma_z>_p0(t=0)=0.2
+     Thus, here: 3=predator; 1=prey.
+
+Individual 2 interacts with individual 3.
+     2:  <sigma_z>_p0(t=0)=-0.6
+     3:  <sigma_z>_p0(t=0)=0.2
+     Thus, here: 3=predator; 2=prey.
+
+Individual 3 interacts with individual 1.
+     3:  <sigma_z>_p0(t=0)=0.2
+     1:  <sigma_z>_p0(t=0)=-2.8
+     Thus, here: 3=predator; 1=prey.
+
+Individual 3 interacts with individual 2.
+     3:  <sigma_z>_p0(t=0)=0.2
+     2:  <sigma_z>_p0(t=0)=-0.6
+     Thus, here: 3=predator; 2=prey.
+
+
+
+
+EFFECTS:
+
+If  3=predator; 1=prey:
+     <H_int>_genotypes = 1.9212
+     The predator (3) HUNTS the prey (1):      <sigma_z>_p0^pred=0.73794;   <sigma_z>_p0^prey=-3.3379.
+
+If  3=predator; 2=prey:
+     <H_int>_genotypes = 0.5516
+     The predator (3) HUNTS the prey (2):      <sigma_z>_p0^pred=0.77103;   <sigma_z>_p0^prey=-0.6331.
+
+
+
+
+
+
+
+
+
+
+
+-------------------------------------At t=0.1--------------------------------------
+
+
+Individual 3 has DIED!    (Death age=0.7)
+Then, individual 3 disappears from the grid:
+   0   2   0   1
+   0   0   0   0
+   0   0   0   0
+
+
+____________________________________AGE___________________________________
+     1:  <sigma_z>_p0=-2.4384
+     2:  <sigma_z>_p0=-0.44774
+
+
+____________________________Random MOVEMENT_______________________________
+If individual 1 moves in direction (x,y)=(1,0):
+   1   2   0   0
+   0   0   0   0
+   0   0   0   0
+If individual 2 moves in direction (x,y)=(0,1):
+   1   0   0   0
+   0   0   0   0
+   0   2   0   0
+
+
+____________________________ INTERACTION__________________________________
+(<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+
+Individual 1 interacts with individual 2.
+     1:  <sigma_z>_p0(t=0)=-2.8
+     2:  <sigma_z>_p0(t=0)=-0.6
+     Thus, here: 2=predator; 1=prey.
+
+Individual 2 interacts with individual 1.
+     2:  <sigma_z>_p0(t=0)=-0.6
+     1:  <sigma_z>_p0(t=0)=-2.8
+     Thus, here: 2=predator; 1=prey.
+
+
+
+
+EFFECTS:
+
+If  2=predator; 1=prey:
+     <H_int>_genotypes = -0.082
+     The prey (1) ESCAPES from the prey (1), so it does NOT get any harm!
+
+
+
+
+
+
+
+
+
+
+
+-------------------------------------At t=0.2--------------------------------------
+
+
+
+____________________________________AGE___________________________________
+     1:  <sigma_z>_p0=-1.8151
+     2:  <sigma_z>_p0=-0.18531
+
+
+____________________________Random MOVEMENT_______________________________
+If individual 1 moves in direction (x,y)=(-1,0):
+   0   0   0   1
+   0   0   0   0
+   0   2   0   0
+If individual 2 moves in direction (x,y)=(0,1):
+   0   0   0   1
+   0   2   0   0
+   0   0   0   0
+
+
+____________________________ INTERACTION__________________________________
+(<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+
+
+
+
+EFFECTS:
+
+
+
+
+
+
+
+
+
+
+
+-------------------------------------At t=0.3--------------------------------------
+
+
+
+____________________________________AGE___________________________________
+     1:  <sigma_z>_p0=-1.0855
+     2:  <sigma_z>_p0=0.1219
+
+
+____________________________Random MOVEMENT_______________________________
+If individual 1 moves in direction (x,y)=(0,1):
+   0   0   0   0
+   0   2   0   0
+   0   0   0   1
+If individual 2 moves in direction (x,y)=(0,0):
+   0   0   0   0
+   0   2   0   0
+   0   0   0   1
+
+
+____________________________ INTERACTION__________________________________
+(<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+
+
+
+
+EFFECTS:
+
+
+
+
+
+
+
+
+
+
+
+-------------------------------------At t=0.4--------------------------------------
+
+
+
+____________________________________AGE___________________________________
+     1:  <sigma_z>_p0=-0.39794
+     2:  <sigma_z>_p0=0.41139
+
+
+____________________________Random MOVEMENT_______________________________
+If individual 1 moves in direction (x,y)=(-1,-1):
+   0   0   1   0
+   0   2   0   0
+   0   0   0   0
+If individual 2 moves in direction (x,y)=(1,1):
+Individual 2 canNOT move there, that cell is already occupied by individual 1.
+   0   0   1   0
+   0   2   0   0
+   0   0   0   0
+
+
+____________________________ INTERACTION__________________________________
+(<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+
+Individual 1 interacts with individual 2.
+     1:  <sigma_z>_p0(t=0)=-2.8
+     2:  <sigma_z>_p0(t=0)=-0.6
+     Thus, here: 2=predator; 1=prey.
+
+Individual 2 interacts with individual 1.
+     2:  <sigma_z>_p0(t=0)=-0.6
+     1:  <sigma_z>_p0(t=0)=-2.8
+     Thus, here: 2=predator; 1=prey.
+
+
+
+
+EFFECTS:
+
+If  2=predator; 1=prey:
+     <H_int>_genotypes = -0.082
+     The prey (1) ESCAPES from the prey (1), so it does NOT get any harm!
+
+
+
+
+
+
+
+
+
+
+
+-------------------------------------At t=0.5--------------------------------------
+
+
+
+____________________________________AGE___________________________________
+     1:  <sigma_z>_p0=0.15211
+     2:  <sigma_z>_p0=0.64299
+
+
+____________________________Random MOVEMENT_______________________________
+If individual 1 moves in direction (x,y)=(0,1):
+   0   0   0   0
+   0   2   0   0
+   0   0   1   0
+If individual 2 moves in direction (x,y)=(1,1):
+   0   0   2   0
+   0   0   0   0
+   0   0   1   0
+
+
+____________________________ INTERACTION__________________________________
+(<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+
+Individual 1 interacts with individual 2.
+     1:  <sigma_z>_p0(t=0)=-2.8
+     2:  <sigma_z>_p0(t=0)=-0.6
+     Thus, here: 2=predator; 1=prey.
+
+Individual 2 interacts with individual 1.
+     2:  <sigma_z>_p0(t=0)=-0.6
+     1:  <sigma_z>_p0(t=0)=-2.8
+     Thus, here: 2=predator; 1=prey.
+
+
+
+
+EFFECTS:
+
+If  2=predator; 1=prey:
+     <H_int>_genotypes = -0.082
+     The prey (1) ESCAPES from the prey (1), so it does NOT get any harm!
+
+
+
+
+
+
+
+
+
+
+
+-------------------------------------At t=0.6--------------------------------------
+
+
+
+____________________________________AGE___________________________________
+     1:  <sigma_z>_p0=0.53467
+     2:  <sigma_z>_p0=0.80407
+
+
+____________________________Random MOVEMENT_______________________________
+If individual 1 moves in direction (x,y)=(-1,-1):
+   0   1   2   0
+   0   0   0   0
+   0   0   0   0
+If individual 2 moves in direction (x,y)=(1,1):
+   0   1   0   0
+   0   0   0   0
+   0   0   0   2
+
+
+____________________________ INTERACTION__________________________________
+(<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+
+
+
+
+EFFECTS:
+
+
+
+
+
+
+
+
+
+
+
+-------------------------------------At t=0.7--------------------------------------
+
+
+Individual 2 has DIED!    (Death age=0.7)
+Then, individual 2 disappears from the grid:
+   0   1   0   0
+   0   0   0   0
+   0   0   0   0
+
+
+____________________________________AGE___________________________________
+     1:  <sigma_z>_p0=0.76892
+
+
+____________________________Random MOVEMENT_______________________________
+If individual 1 moves in direction (x,y)=(1,0):
+   0   0   1   0
+   0   0   0   0
+   0   0   0   0
+
+
+____________________________ INTERACTION__________________________________
+(<sigma_z>_p(t=0)=<sigma_z>_g=predatorness)
+
+
+
+
+EFFECTS:
+
+
+
+
+
+
+
+
+
+
+
+-------------------------------------At t=0.8--------------------------------------
+
+
+Individual 1 has DIED!    (Death age=0.7)
+Then, individual 1 disappears from the grid:
+   0   0   0   0
+   0   0   0   0
+   0   0   0   0
+
+
+
+ALL individuals are dead, program ENDS!
