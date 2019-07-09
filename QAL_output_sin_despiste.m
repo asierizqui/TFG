dedff6e25a30a2e753cc769baf3f00fe2ae02bd5
Asier Izquierdo <asierizqui@gmail.com>
2019-07-09 21:19:16 +0200

    Add files via upload

---
+++ QAL_output_sin_despiste.m
@@ -0,0 +1,444 @@
+________________________________GRID at t=0______________________________________
+   0   0   0   1
+   2   0   0   0
+   0   0   3   0
+
+
+
+
+
+
+
+-------------------------------------At t=0--------------------------------------
+
+
+
+____________________________________AGE___________________________________
+     1:  <sigma_z>_p0=-0.8
+     2:  <sigma_z>_p0=-1.4
+     3:  <sigma_z>_p0=0.2
+
+
+____________________________Random MOVEMENT_______________________________
+     NO movement at t=0.
+
+
+____________________________ INTERACTION__________________________________
+(<sigma_z>_p(t=0)=<sigma_z>_g=trophic role)
+
+Individual 1 interacts with individual 3.
+     1:  <sigma_z>_p0(t=0)=-0.8
+     3:  <sigma_z>_p0(t=0)=0.2
+     Thus, here: 3=predator; 1=prey.
+
+Individual 1 interacts with individual 2.
+     1:  <sigma_z>_p0(t=0)=-0.8
+     2:  <sigma_z>_p0(t=0)=-1.4
+     Thus, here: 1=predator; 2=prey.
+
+Individual 2 interacts with individual 1.
+     2:  <sigma_z>_p0(t=0)=-1.4
+     1:  <sigma_z>_p0(t=0)=-0.8
+     Thus, here: 1=predator; 2=prey.
+
+Individual 3 interacts with individual 1.
+     3:  <sigma_z>_p0(t=0)=0.2
+     1:  <sigma_z>_p0(t=0)=-0.8
+     Thus, here: 3=predator; 1=prey.
+
+
+
+
+EFFECTS:
+
+If  1=predator; 2=prey:
+     <H_int>_genotypes = -1.0248
+     The prey (2) ESCAPES from the predator (1), so it does NOT get any harm!
+
+If  3=predator; 1=prey:
+     <H_int>_genotypes = -0.0016
+     The prey (1) ESCAPES from the predator (3), so it does NOT get any harm!
+
+
+
+
+
+
+
+
+
+
+
+-------------------------------------At t=0.1--------------------------------------
+
+
+
+____________________________________AGE___________________________________
+     1:  <sigma_z>_p0=-0.62871
+     2:  <sigma_z>_p0=-1.1716
+     3:  <sigma_z>_p0=0.27613
+
+
+____________________________Random MOVEMENT_______________________________
+If individual 1 moves in direction (x,y)=(0,1):
+   0   0   0   0
+   2   0   0   0
+   0   0   3   1
+If individual 2 moves in direction (x,y)=(1,0):
+   0   0   0   0
+   0   2   0   0
+   0   0   3   1
+If individual 3 moves in direction (x,y)=(-1,0):
+   0   0   0   0
+   0   2   0   0
+   0   3   0   1
+
+
+____________________________ INTERACTION__________________________________
+(<sigma_z>_p(t=0)=<sigma_z>_g=trophic role)
+
+Individual 2 interacts with individual 3.
+     2:  <sigma_z>_p0(t=0)=-1.4
+     3:  <sigma_z>_p0(t=0)=0.2
+     Thus, here: 3=predator; 2=prey.
+
+Individual 3 interacts with individual 2.
+     3:  <sigma_z>_p0(t=0)=0.2
+     2:  <sigma_z>_p0(t=0)=-1.4
+     Thus, here: 3=predator; 2=prey.
+
+
+
+
+EFFECTS:
+
+If  3=predator; 2=prey:
+     <H_int>_genotypes = -1.5344
+     The prey (2) ESCAPES from the predator (3), so it does NOT get any harm!
+
+
+
+
+
+
+
+
+
+
+
+-------------------------------------At t=0.2--------------------------------------
+
+
+
+____________________________________AGE___________________________________
+     1:  <sigma_z>_p0=-0.33347
+     2:  <sigma_z>_p0=-0.77796
+     3:  <sigma_z>_p0=0.40735
+
+
+____________________________Random MOVEMENT_______________________________
+If individual 1 moves in direction (x,y)=(1,-1):
+   1   0   0   0
+   0   2   0   0
+   0   3   0   0
+If individual 2 moves in direction (x,y)=(-1,1):
+Individual 2 canNOT move there, that cell is already occupied by individual 1.
+   1   0   0   0
+   0   2   0   0
+   0   3   0   0
+If individual 3 moves in direction (x,y)=(1,1):
+   1   0   0   0
+   0   2   3   0
+   0   0   0   0
+
+
+____________________________ INTERACTION__________________________________
+(<sigma_z>_p(t=0)=<sigma_z>_g=trophic role)
+
+Individual 1 interacts with individual 2.
+     1:  <sigma_z>_p0(t=0)=-0.8
+     2:  <sigma_z>_p0(t=0)=-1.4
+     Thus, here: 1=predator; 2=prey.
+
+Individual 2 interacts with individual 1.
+     2:  <sigma_z>_p0(t=0)=-1.4
+     1:  <sigma_z>_p0(t=0)=-0.8
+     Thus, here: 1=predator; 2=prey.
+
+Individual 2 interacts with individual 3.
+     2:  <sigma_z>_p0(t=0)=-1.4
+     3:  <sigma_z>_p0(t=0)=0.2
+     Thus, here: 3=predator; 2=prey.
+
+Individual 3 interacts with individual 2.
+     3:  <sigma_z>_p0(t=0)=0.2
+     2:  <sigma_z>_p0(t=0)=-1.4
+     Thus, here: 3=predator; 2=prey.
+
+
+
+
+EFFECTS:
+
+If  1=predator; 2=prey:
+     <H_int>_genotypes = -1.0248
+     The prey (2) ESCAPES from the predator (1), so it does NOT get any harm!
+
+If  3=predator; 2=prey:
+     <H_int>_genotypes = -1.5344
+     The prey (2) ESCAPES from the predator (3), so it does NOT get any harm!
+
+
+
+
+
+
+
+
+
+
+
+-------------------------------------At t=0.3--------------------------------------
+
+
+
+____________________________________AGE___________________________________
+     1:  <sigma_z>_p0=0.012139
+     2:  <sigma_z>_p0=-0.31715
+     3:  <sigma_z>_p0=0.56095
+
+
+____________________________Random MOVEMENT_______________________________
+If individual 1 moves in direction (x,y)=(1,-1):
+Individual 1 canNOT move there, that cell is already occupied by individual 2.
+   1   0   0   0
+   0   2   3   0
+   0   0   0   0
+If individual 2 moves in direction (x,y)=(1,-1):
+   1   0   0   0
+   0   0   3   0
+   0   0   2   0
+If individual 3 moves in direction (x,y)=(1,1):
+   1   0   0   3
+   0   0   0   0
+   0   0   2   0
+
+
+____________________________ INTERACTION__________________________________
+(<sigma_z>_p(t=0)=<sigma_z>_g=trophic role)
+
+Individual 1 interacts with individual 3.
+     1:  <sigma_z>_p0(t=0)=-0.8
+     3:  <sigma_z>_p0(t=0)=0.2
+     Thus, here: 3=predator; 1=prey.
+
+Individual 2 interacts with individual 3.
+     2:  <sigma_z>_p0(t=0)=-1.4
+     3:  <sigma_z>_p0(t=0)=0.2
+     Thus, here: 3=predator; 2=prey.
+
+Individual 3 interacts with individual 2.
+     3:  <sigma_z>_p0(t=0)=0.2
+     2:  <sigma_z>_p0(t=0)=-1.4
+     Thus, here: 3=predator; 2=prey.
+
+Individual 3 interacts with individual 1.
+     3:  <sigma_z>_p0(t=0)=0.2
+     1:  <sigma_z>_p0(t=0)=-0.8
+     Thus, here: 3=predator; 1=prey.
+
+
+
+
+EFFECTS:
+
+If  3=predator; 1=prey:
+     <H_int>_genotypes = -0.0016
+     The prey (1) ESCAPES from the predator (3), so it does NOT get any harm!
+
+If  3=predator; 2=prey:
+     <H_int>_genotypes = -1.5344
+     The prey (2) ESCAPES from the predator (3), so it does NOT get any harm!
+
+
+
+
+
+
+
+
+
+
+
+-------------------------------------At t=0.4--------------------------------------
+
+
+
+____________________________________AGE___________________________________
+     1:  <sigma_z>_p0=0.33782
+     2:  <sigma_z>_p0=0.11709
+     3:  <sigma_z>_p0=0.7057
+
+
+____________________________Random MOVEMENT_______________________________
+If individual 1 moves in direction (x,y)=(0,-1):
+   0   0   0   3
+   1   0   0   0
+   0   0   2   0
+If individual 2 moves in direction (x,y)=(0,0):
+   0   0   0   3
+   1   0   0   0
+   0   0   2   0
+If individual 3 moves in direction (x,y)=(1,0):
+   3   0   0   0
+   1   0   0   0
+   0   0   2   0
+
+
+____________________________ INTERACTION__________________________________
+(<sigma_z>_p(t=0)=<sigma_z>_g=trophic role)
+
+Individual 1 interacts with individual 3.
+     1:  <sigma_z>_p0(t=0)=-0.8
+     3:  <sigma_z>_p0(t=0)=0.2
+     Thus, here: 3=predator; 1=prey.
+
+Individual 3 interacts with individual 1.
+     3:  <sigma_z>_p0(t=0)=0.2
+     1:  <sigma_z>_p0(t=0)=-0.8
+     Thus, here: 3=predator; 1=prey.
+
+
+
+
+EFFECTS:
+
+If  3=predator; 1=prey:
+     <H_int>_genotypes = -0.0016
+     The prey (1) ESCAPES from the predator (3), so it does NOT get any harm!
+
+
+
+
+
+
+
+
+
+
+
+-------------------------------------At t=0.5--------------------------------------
+
+
+Individual 3 has DIED!    (Death age=0.7)
+Then, individual 3 disappears from the grid:
+   0   0   0   0
+   1   0   0   0
+   0   0   2   0
+
+
+____________________________________AGE___________________________________
+     1:  <sigma_z>_p0=0.59837
+     2:  <sigma_z>_p0=0.46449
+
+
+____________________________Random MOVEMENT_______________________________
+If individual 1 moves in direction (x,y)=(0,-1):
+   0   0   0   0
+   0   0   0   0
+   1   0   2   0
+If individual 2 moves in direction (x,y)=(-1,1):
+   0   0   0   0
+   0   2   0   0
+   1   0   0   0
+
+
+____________________________ INTERACTION__________________________________
+(<sigma_z>_p(t=0)=<sigma_z>_g=trophic role)
+
+Individual 1 interacts with individual 2.
+     1:  <sigma_z>_p0(t=0)=-0.8
+     2:  <sigma_z>_p0(t=0)=-1.4
+     Thus, here: 1=predator; 2=prey.
+
+Individual 2 interacts with individual 1.
+     2:  <sigma_z>_p0(t=0)=-1.4
+     1:  <sigma_z>_p0(t=0)=-0.8
+     Thus, here: 1=predator; 2=prey.
+
+
+
+
+EFFECTS:
+
+If  1=predator; 2=prey:
+     <H_int>_genotypes = -1.0248
+     The prey (2) ESCAPES from the predator (1), so it does NOT get any harm!
+
+
+
+
+
+
+
+
+
+
+
+-------------------------------------At t=0.6--------------------------------------
+
+
+
+____________________________________AGE___________________________________
+     1:  <sigma_z>_p0=0.77958
+     2:  <sigma_z>_p0=0.7061
+
+
+____________________________Random MOVEMENT_______________________________
+If individual 1 moves in direction (x,y)=(-1,1):
+   0   0   0   0
+   0   2   0   1
+   0   0   0   0
+If individual 2 moves in direction (x,y)=(0,0):
+   0   0   0   0
+   0   2   0   1
+   0   0   0   0
+
+
+____________________________ INTERACTION__________________________________
+(<sigma_z>_p(t=0)=<sigma_z>_g=trophic role)
+
+
+
+
+EFFECTS:
+
+
+
+
+
+
+
+
+
+
+
+-------------------------------------At t=0.7--------------------------------------
+
+
+Individual 1 has DIED!    (Death age=0.7)
+Then, individual 1 disappears from the grid:
+   0   0   0   0
+   0   2   0   0
+   0   0   0   0
+
+Individual 2 has DIED!    (Death age=0.7)
+Then, individual 2 disappears from the grid:
+   0   0   0   0
+   0   0   0   0
+   0   0   0   0
+
+
+
+
+ALL individuals are dead, program ENDS!
